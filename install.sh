a474909a8ef3aaaab2a3afec1bcc543307b067bc
Pieter <pieter@tuxie.com>
2015-12-07 16:33:06 +0100

    Add: Install Vundle from Git. Add: Ask to install Hack-Regular.

--- install.sh
+++ install.sh
@@ -4,15 +4,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
 for file in $(ls -1a | grep "^\.[a-z][a-z\._-]\+$" ); do
-  if [ "$file" == ".git" ]; then
+  if [ "$file" == ".git" ] || [ "$file" == ".fonts" ] ; then
     continue
   fi
-  if [ -f "$HOME/$file" ] || [ -h "$HOME/$file" ] || [ -d "$HOME/$file" ]
-  then
+  if [ -f "$HOME/$file" ] || [ -h "$HOME/$file" ] || [ -d "$HOME/$file" ]; then
     echo -n "$HOME/$file already exists. do you want to overwrite? this will remove the existing file or folder. (Y/n): "
     read response
-    if [ "$response" == "Y" ] || [ "$response" == "y" ] || [ "$response" == "" ]
-    then
+    if [ "$response" == "Y" ] || [ "$response" == "y" ] || [ "$response" == "" ]; then
       rm -rf $HOME/$file
       echo "Removing $HOME/$file.."
       echo "Installing $HOME/$file"
@@ -24,9 +22,20 @@ for file in $(ls -1a | grep "^\.[a-z][a-z\._-]\+$" ); do
   fi
 done
 
-echo "Updating font cache .."
-fc-cache -f
+echo " "
+echo "Installing Vundle ... "
+git clone https://github.com/VundleVim/Vundle.vim.git $DIR/.vim/bundle/Vundle.vim
 
+echo " "
+echo -n "Install Hack-Regular font? (Y/n): "
+read response
+if [ "$response" == "Y" ] || [ "$response" == "y" ] || [ "$response" == "" ]; then
+  ln -s $DIR/.fonts $HOME/.fonts
+  echo "Updating font cache .."
+  fc-cache -f
+fi
+
+echo " "
 echo -n "Install Solarized profile for Xfce4 terminal? (Y/n): "
 read response
 if [ "$response" == "Y" ] || [ "$response" == "y" ] || [ "$response" == "" ]; then
@@ -34,5 +43,6 @@ if [ "$response" == "Y" ] || [ "$response" == "y" ] || [ "$response" == "" ]; th
   ln -s $DIR/terminalrc $HOME/.config/xfce4/terminal/terminalrc
 fi
 
+echo " "
 echo "All done :)"
 echo " "
